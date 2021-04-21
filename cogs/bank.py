7fc9d0a593e258a53593781ed08b97725f1fa8f8
PyCord#3494 <wilsonis123@icloud.com>
2021-04-20 17:11:57 -0700

    Deposit and withdraw from bank! :)

--- cogs/bank.py
+++ cogs/bank.py
@@ -4,11 +4,21 @@ import pymysql
 import asyncio
 import config
 
+from math import floor
+
 class Bank(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.items = [1000, 5000, 25000, 100000, 150000, 250000, 20000]
 		self.coin = "<:coins:585233801320333313>"
+
+	@commands.command(aliases=['dep', 'd'])
+	async def deposit(self, ctx):
+		await ctx.invoke(self.bot.get_command('help bank'))
+
+	@commands.command(aliases=['w'])
+	async def withdraw(self, ctx):
+		await ctx.invoke(self.bot.get_command('help bank'))
 
 	@commands.group(invoke_without_command=True)
 	async def bank(self, ctx):
@@ -16,8 +26,20 @@ class Bank(commands.Cog):
 			# await ctx.send("`bank <deposit/withdraw> amount`")
 			await ctx.invoke(self.bot.get_command('help bank'))
 
-	@bank.command()
-	async def deposit(self, ctx, amnt:int):
+	@bank.command(name='deposit', aliases=['dep', 'd'])
+	@commands.cooldown(1, 3, commands.BucketType.user)
+	async def _deposit(self, ctx, amnt):
+		try:
+			amnt = int(amnt)
+		except:
+			if amnt == "all" or amnt == "max":
+				amnt = await self.bot.get_cog("Economy").getBalance(ctx.author)
+			elif amnt == "half":
+				amnt = floor(await self.bot.get_cog("Economy").getBalance(ctx.author)/2)
+			else:
+				await ctx.send("Incorrect withdrawal amount.")
+				return
+
 		if not await self.bot.get_cog("Economy").subtractBet(ctx.author, amnt):
 			await ctx.send("You either don't have enough or you haven't made an account with `.start`")
 			return
@@ -34,13 +56,24 @@ class Bank(commands.Cog):
 		await ctx.send(f"Successfully deposited {amnt}{self.coin}!")
 
 
-	@bank.command()
-	async def withdraw(self, ctx, amnt):
-		if amnt < 0:
-			await ctx.send("You cannot withdraw a negative amount.")
-			return
-		if self.getBankBal(ctx.author.id) < amnt:
-			await ctx.send("You do not have enough funds in your bank to withdraw that amount.")
+	@bank.command(name='withdraw', aliases=['w'])
+	@commands.cooldown(1, 3, commands.BucketType.user)
+	async def _withdraw(self, ctx, amnt):
+		try:
+			amnt = int(amnt)
+			if self.getBankBal(ctx.author.id) < amnt:
+				await ctx.send("You do not have enough funds in your bank to withdraw that amount.")
+				return
+		except:
+			if amnt == "all":
+				amnt = self.getBankBal(ctx.author.id)
+			elif amnt == "half":
+				amnt = floor(self.getBankBal(ctx.author.id) / 2)
+			else:
+				await ctx.send("Incorrect withdrawal amount.")
+				return
+		if amnt <= 0:
+			await ctx.send("You must withdraw an amount greater than 0.")
 			return
 
 		db = pymysql.connect(host=config.host, port=3306, user=config.user, passwd=config.passwd, db=config.db, autocommit=True)
@@ -57,7 +90,7 @@ class Bank(commands.Cog):
 		await ctx.send(f"Successfully withdrew {amnt}{self.coin}!")
 
 
-	@bank.command(aliases=['status'])
+	@bank.command(aliases=['status', 'stat', 'stats'])
 	async def balance(self, ctx):
 		await ctx.send(f"You have {self.getBankBal(ctx.author.id)}{self.coin} in your bank.")
 
