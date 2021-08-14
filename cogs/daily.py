8fb6f9169b05e9282d7c1240c0d7a4a7e5cd6f20
PyCord3494 <wilsonis123@icloud.com>
2021-08-13 19:50:43 -0700

    Added weekly/monthly commands. Daily cooldown will reset if user does not have account

--- cogs/daily.py
+++ cogs/daily.py
@@ -15,6 +15,7 @@ class Daily(commands.Cog):
 	async def daily(self, ctx):
 		if not await self.bot.get_cog("Economy").accCheck(ctx.author):
 			await ctx.send("Hello! Please type .start to create your wallet. :smiley:")
+			ctx.command.reset_cooldown(ctx)
 			return			
 		dailyReward = await self.getDailyReward(ctx)
 		multiplier = self.bot.get_cog("Economy").getMultiplier(ctx.author)
@@ -38,6 +39,41 @@ class Daily(commands.Cog):
 		db.close()
 		dailyReward = getRow[0]
 		return dailyReward
+
+
+	@commands.command()
+	@commands.cooldown(1, 604800, commands.BucketType.user)
+	async def weekly(self, ctx):
+		if not await self.bot.get_cog("Economy").accCheck(ctx.author):
+			await ctx.send("Hello! Please type .start to create your wallet. :smiley:")
+			ctx.command.reset_cooldown(ctx)
+			return
+		weeklyReward = 12500
+		multiplier = self.bot.get_cog("Economy").getMultiplier(ctx.author)
+		extraMoney = int(weeklyReward * (multiplier - 1))
+		await self.bot.get_cog("Economy").addWinnings(ctx.author.id, weeklyReward + extraMoney)
+		balance = await self.bot.get_cog("Economy").getBalance(ctx.author)
+		embed = discord.Embed(color=1768431)
+		embed.add_field(name = f"You got {weeklyReward} (+{extraMoney}) {self.coin}", 
+						value = f"You have {balance} credits\nMultiplier: {multiplier}x\nExtra Money: {extraMoney}", inline=False)
+		await ctx.send(embed=embed)
+
+	@commands.command()
+	@commands.cooldown(1, 2592000, commands.BucketType.user)
+	async def monthly(self, ctx):
+		if not await self.bot.get_cog("Economy").accCheck(ctx.author):
+			await ctx.send("Hello! Please type .start to create your wallet. :smiley:")
+			ctx.command.reset_cooldown(ctx)
+			return
+		monthlyReward = 36000
+		multiplier = self.bot.get_cog("Economy").getMultiplier(ctx.author)
+		extraMoney = int(monthlyReward * (multiplier - 1))
+		await self.bot.get_cog("Economy").addWinnings(ctx.author.id, monthlyReward + extraMoney)
+		balance = await self.bot.get_cog("Economy").getBalance(ctx.author)
+		embed = discord.Embed(color=1768431)
+		embed.add_field(name = f"You got {monthlyReward} (+{extraMoney}) {self.coin}", 
+						value = f"You have {balance} credits\nMultiplier: {multiplier}x\nExtra Money: {extraMoney}", inline=False)
+		await ctx.send(embed=embed)
 
 def setup(bot):
 	bot.add_cog(Daily(bot))
