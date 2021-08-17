c21753303ac146a864e46d37318c409caa9d2101
PyCord3494 <wilsonis123@icloud.com>
2021-08-17 01:29:24 -0700

    More embeds! Unregistered users now auto-registered

--- cogs/shop.py
+++ cogs/shop.py
@@ -15,26 +15,26 @@ class Shop(commands.Cog):
 
 	@commands.group(invoke_without_command=True)
 	async def shop(self, ctx):
-		if await self.bot.get_cog("Economy").accCheck(ctx.author):
-			if ctx.invoked_subcommand is None:
-				await ctx.send("```ML\nID            ITEMS                COST\n"
-									+ "----------------------------------------\n"
-									+ "1            1 crate               1,000\n"
-									+ "2             1 key                5,000\n"
-									 + "3        +1,000 to daily          25,000\n\n"
-									# + "4       +500 to lvl reward       100,000\n"
-									# + "5      ---------------------     150,000\n"
-									# + "6   +1.5x Profit on Future Games 250,000\n"
-									 + "7     +1,500 to donator reward    20,000\n"
-									+ "----------------------------------------\n"
-									+ "Use .shop buy <id> <amount>\n\n\tSHOP IS A WORK IN PROGRESS!```")
-
-		else:
-			await ctx.send("Hello! Please type .start to create your wallet. :smiley:")
-			return 0
+		if not await self.bot.get_cog("Economy").accCheck(ctx.author):
+			await ctx.invoke(self.bot.get_command('start'))
+			
+		if ctx.invoked_subcommand is None:
+			await ctx.send("```ML\nID            ITEMS                COST\n"
+								+ "----------------------------------------\n"
+								+ "1            1 crate               1,000\n"
+								+ "2             1 key                5,000\n"
+								+ "3      +1,000 to daily reward     25,000\n\n"
+								# + "4       +500 to lvl reward       100,000\n"
+								# + "5      ---------------------     150,000\n"
+								# + "6   +1.5x Profit on Future Games 250,000\n"
+								+ "7     +1,500 to donator reward    20,000\n"
+								+ "----------------------------------------\n"
+								+ "Use .shop buy <id> <amount>\n\n\tSHOP IS A WORK IN PROGRESS!```")
 
 	@shop.command()
 	async def buy(self, ctx, ID, amnt: int=1):
+		if not await self.bot.get_cog("Economy").accCheck(ctx.author):
+			await ctx.invoke(self.bot.get_command('start')) 
 		try:
 			ID = int(ID)
 		except:
@@ -51,96 +51,108 @@ class Shop(commands.Cog):
 			return
 		if ID == 7 and not self.bot.get_cog("Economy").isDonator(ctx.author.id):
 			embed = discord.Embed(color=1768431)
+			embed.set_thumbnail(url=ctx.author.avatar_url)
 			embed.add_field(name = f"Shop", 
 							value = f"That can only be used by Donators! To donate, type .donate", inline=False)
+			embed.set_footer(text=f"{ctx.author}")
 			await ctx.send(embed=embed)
 			return
 
-		if await self.bot.get_cog("Economy").accCheck(ctx.author):
-			discordId = ctx.author.id
-			cost = self.items[ID - 1] * amnt
-			balance = await self.bot.get_cog("Economy").getBalance(ctx.author)
-			
-			if balance < cost:
-				await ctx.send(f"That will cost you {cost}{self.coin}, but you only have {balance}{self.coin}")
-				return
+		discordId = ctx.author.id
+		cost = self.items[ID - 1] * amnt
+		balance = await self.bot.get_cog("Economy").getBalance(ctx.author)
+		
+		if balance < cost:
+			embed = discord.Embed(color=1768431, title=f"{self.bot.user.name} | Shop")
+			embed.set_thumbnail(url=ctx.author.avatar_url)
+			embed.add_field(name="Error", value=f"That will cost you {cost}{self.coin}, but you only have {balance}{self.coin}")
+			embed.set_footer(text=f"{ctx.author}")
+			await ctx.send(embed=embed)
+			return
 
-			await self.bot.get_cog("Economy").addWinnings(discordId, -(cost))
-			db = pymysql.connect(host=config.host, port=3306, user=config.user, passwd=config.passwd, db=config.db, autocommit=True)
-			cursor = db.cursor()
-			
-			if ID == 1:
-				sql = f"""UPDATE Inventory
-					  SET Crates = Crates + {amnt}
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()
-				await ctx.invoke(self.bot.get_command('balance'))
+		await self.bot.get_cog("Economy").addWinnings(discordId, -(cost))
+		db = pymysql.connect(host=config.host, port=3306, user=config.user, passwd=config.passwd, db=config.db, autocommit=True)
+		cursor = db.cursor()
+		
+		if ID == 1:
+			sql = f"""UPDATE Inventory
+				  SET Crates = Crates + {amnt}
+				  WHERE DiscordID = '{discordId}';"""
+			cursor.execute(sql)
+			db.commit()
+			await ctx.invoke(self.bot.get_command('balance'))
 
-			elif ID == 2:
-				sql = f"""UPDATE Inventory
-					  SET Keyss = Keyss + {amnt}
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()
-				await ctx.invoke(self.bot.get_command('balance'))
+		elif ID == 2:
+			sql = f"""UPDATE Inventory
+				  SET Keyss = Keyss + {amnt}
+				  WHERE DiscordID = '{discordId}';"""
+			cursor.execute(sql)
+			db.commit()
+			await ctx.invoke(self.bot.get_command('balance'))
 
-			elif ID == 3:
-				sql = f"""UPDATE Economy
-					  SET DailyReward = DailyReward + {amnt * 1000}
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()	
+		elif ID == 3:
+			sql = f"""UPDATE Economy
+				  SET DailyReward = DailyReward + {amnt * 1000}
+				  WHERE DiscordID = '{discordId}';"""
+			cursor.execute(sql)
+			db.commit()	
 
-				embed = discord.Embed(color=1768431)
-				embed.add_field(name = f"Shop", 
-								value = f"Successfully added {amnt * 1000}{self.coin} to daily reward")
-				await ctx.send(embed=embed)
+			embed = discord.Embed(color=1768431)
+			embed.set_thumbnail(url=ctx.author.avatar_url)
+			embed.set_footer(text=f"{ctx.author}")
+			embed.add_field(name = f"Shop", 
+							value = f"Successfully added {amnt * 1000}{self.coin} to daily reward")
+			await ctx.send(embed=embed)
 
-			# elif ID == 4:
-			# 	sql = f"""UPDATE Economy
-			# 		  SET LevelReward = LevelReward + 500
-			# 		  WHERE DiscordID = '{discordId}';"""
-			# 	cursor.execute(sql)
-			# 	db.commit()	
+		# elif ID == 4:
+		# 	sql = f"""UPDATE Economy
+		# 		  SET LevelReward = LevelReward + 500
+		# 		  WHERE DiscordID = '{discordId}';"""
+		# 	cursor.execute(sql)
+		# 	db.commit()	
 
-			# elif ID == 5:
-			# 	pass
+		# elif ID == 5:
+		# 	pass
 
-			# elif ID == 6:
-			# 	sql = f"""UPDATE Economy
-			# 		  SET Multiplier = Multiplier + 0.5
-			# 		  WHERE DiscordID = '{discordId}';"""
-			# 	cursor.execute(sql)
-			# 	db.commit()	
+		# elif ID == 6:
+		# 	sql = f"""UPDATE Economy
+		# 		  SET Multiplier = Multiplier + 0.5
+		# 		  WHERE DiscordID = '{discordId}';"""
+		# 	cursor.execute(sql)
+		# 	db.commit()	
 
-			elif ID == 7:
-				sql = f"""UPDATE Economy
-					  SET DonatorReward = DonatorReward + {amnt * 1500}
-					  WHERE DiscordID = '{discordId}';"""
-				cursor.execute(sql)
-				db.commit()	
+		elif ID == 7:
+			sql = f"""UPDATE Economy
+				  SET DonatorReward = DonatorReward + {amnt * 1500}
+				  WHERE DiscordID = '{discordId}';"""
+			cursor.execute(sql)
+			db.commit()	
 
-				embed = discord.Embed(color=1768431)
-				embed.add_field(name = f"Shop", 
-								value = f"Successfully added {amnt * 1000}{self.coin} to donator reward")
-				await ctx.send(embed=embed)
+			embed = discord.Embed(color=1768431)
+			embed.add_field(name = f"Shop", 
+							value = f"Successfully added {amnt * 1000}{self.coin} to donator reward")
+			embed.set_thumbnail(url=ctx.author.avatar_url)
+			embed.set_footer(text=f"{ctx.author}")
+			await ctx.send(embed=embed)
 
 
-			db.close()
-		else:
-			await ctx.send("Hello! Please type $start to create your wallet. :smiley:")
+		db.close()
 
 
 	@shop.command()
-	async def sell(ctx, ID: int, amnt: int):
+	async def sell(self, ctx, ID: int, amnt: int=1):
 		await ctx.send("WORK IN PROGRESS!")
 
 	@commands.group(aliases=['open'])
 	async def crate(self, ctx):
 		# await ctx.send(".crate open 1*")
 		if ctx.invoked_subcommand is None:
-			await ctx.send(f"Open a crate to receive a prize. \nPossible prizes and their chance:\n\t\t**(30%)** 0-3 Crates\n\t\t**(30%)** 1-3 Keys\n\t\t**(20%)** 5000-10000{self.coin}\n\t\t**(20%)** 0 - 500{self.coin}\n**.crate open *amnt***")
+			embed = discord.Embed(color=1768431, title=f"{self.bot.user.name} | Open a crate to receive a prize!")
+			embed.add_field(name="Possible prizes and their chances:", value=f"**(30%)** 0-3 Crates\n**(30%)** 1-3 Keys\n**(20%)** 5000-10000{self.coin}\n**(20%)** 0 - 500{self.coin}\nMore coming soon...")
+			embed.set_footer(text=".crate open")
+			embed.set_thumbnail(url=ctx.author.avatar_url)
+
+			await ctx.send(embed=embed)
 
 
 	@crate.command()
