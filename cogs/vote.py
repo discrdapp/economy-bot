99720236c315901a54b853aeb5a23aaa747f2530
PyCord3494 <wilsonis123@icloud.com>
2021-08-22 16:38:53 -0700

    Vote command! Now works successfully. Users vote to earn a reward

--- cogs/vote.py
+++ cogs/vote.py
@@ -1,37 +1,36 @@
-import dbl
+# import dbl
 import discord
 from discord.ext import commands
 import asyncio
+import random
+import topgg
 
 
 class Vote(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NTIzNTAwMDQ1OTI2NDAwNSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA4Nzc3NzE0fQ.8Bu5bRL6-64ITKOimvi93GSdolUGxyOy5WhLvTmQPi8' # set this to your DBL token
-		self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
-		self.claimAvailable = list()
 
 	@commands.command(description="Vote")
 	@commands.bot_has_guild_permissions(send_messages=True, embed_links=True, use_external_emojis=True)
+	@commands.cooldown(1, 43200, commands.BucketType.user)
 	async def vote(self, ctx):
-		if ctx.author in self.claimAvailable:
-			await ctx.send("Claimed successfully!")
-		else:
-			await ctx.send("You have not voted yet.")
+		token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU4NTIzNTAwMDQ1OTI2NDAwNSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjA4Nzc3NzE0fQ.8Bu5bRL6-64ITKOimvi93GSdolUGxyOy5WhLvTmQPi8' # set this to your DBL token
+		topggpy = topgg.DBLClient(self.bot, token)
 
-	@commands.Cog.listener()
-	async def on_dbl_vote(data):
-		print(data)
-		print(f"User who voted is {data[1]}")
-		self.claimAvailable.append(data[1])
-		# print(f"User who voted is {data.user}")
-
-	@commands.command()
-	async def showUnclaimed(self, ctx):
-		msg = ""
-		for x in self.claimAvailable:
-			msg += f"{x}\n"
-		await ctx.send(f"Claims are available for:\n{msg}")
+		embed = discord.Embed(color=1768431, title=f"{self.bot.user.name} | Vote")
+		embed.set_thumbnail(url=ctx.author.avatar_url)
+		try:
+			if await topggpy.get_user_vote(ctx.author.id):
+				await self.bot.get_cog("Economy").addWinnings(ctx.author.id, 8500)
+				embed.set_thumbnail(url=ctx.author.avatar_url)
+				embed.add_field(name="Thanks for Voting!", value="8500<:coins:585233801320333313> has been added to your account")
+			else:
+				embed.add_field(name="Cannot Claim", value="You have not voted yet.\n[Click here](https://top.gg/bot/585235000459264005/vote/) to vote.")
+				await ctx.send("You have not voted yet.")
+				ctx.command.reset_cooldown(ctx)
+		finally:
+			await topggpy.close()
+		await ctx.send(embed=embed)
 
 def setup(bot):
 	bot.add_cog(Vote(bot))
