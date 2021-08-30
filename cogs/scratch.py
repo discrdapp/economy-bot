896d7bbebc4b453ff8fd5431873323ca71c9498d
PyCord3494 <wilsonis123@icloud.com>
2021-08-29 23:40:39 -0700

    Scratch tickets now work correctly

--- cogs/scratch.py
+++ cogs/scratch.py
@@ -11,42 +11,62 @@ class Scratch(commands.Cog):
 		self.coin = "<:coins:585233801320333313>"
 
 
-	@commands.command(description="Play BlackJack!", aliases=['scratchoff', 'lottery'], pass_context=True)
+	@commands.command(description="Play BlackJack!", aliases=['scratchoff'], pass_context=True)
 	@commands.bot_has_guild_permissions(send_messages=True, manage_messages=True, embed_links=True, use_external_emojis=True, attach_files=True)
 	@commands.cooldown(1, 5, commands.BucketType.user)	
-	async def scratch(self, ctx, amntBet: int):
+	async def scratch(self, ctx, amntBet, skip:str=""):
+
+		if not await self.bot.get_cog("Economy").accCheck(ctx.author):
+			await ctx.invoke(self.bot.get_command('start'))
+
+		amntBet = await self.bot.get_cog("Economy").GetBetAmount(ctx, amntBet)
+
+		if not await self.bot.get_cog("Economy").subtractBet(ctx.author, amntBet):
+			await self.bot.get_cog("Economy").notEnoughMoney(ctx)
+			return
+
 		winningNumber = randint(9,91)
 		n = []
 		for _ in range(0, 9):
-			n.append(randint(winningNumber-2, winningNumber+2))
+			n.append(randint(winningNumber-5, winningNumber+5))
 		spots = {"W1": winningNumber, "A1": n[0], "A2": n[1], "A3": n[2], "B1": n[3], "B2": n[4], "B3": n[5], "C1": n[6], "C2": n[7], "C3": n[8]}
 		
-		msg = ''
-		msg += "Winning number: W1\n"
-		msg += "A1\tA2\tA3\n"
-		msg += "B1\tB2\tB3\n"
-		msg += "C1\tC2\tC3\n"
+		if skip == "":
+			msg = ''
+			msg += "Winning number: W1\n"
+			msg += "A1\tA2\tA3\n"
+			msg += "B1\tB2\tB3\n"
+			msg += "C1\tC2\tC3\n"
 
-		msgSent = await ctx.send(msg)
+			msgSent = await ctx.send(msg)
 
-		count = 0
+			count = 0
 
-		while count < 10:
-			def is_me(m):
-				if m.author.id == ctx.author.id and m.content.upper() in spots.keys():
-					return True
-			try:
-				scratchOff = await self.bot.wait_for('message', check=is_me, timeout=45)
-			except asyncio.TimeoutError:
-				raise Exception("timeoutError")
+			while count < 10:
+				def is_me(m):
+					if m.author.id == ctx.author.id and m.content.upper() in spots.keys():
+						return True
+				try:
+					scratchOff = await self.bot.wait_for('message', check=is_me, timeout=45)
+				except asyncio.TimeoutError:
+					raise Exception("timeoutError")
 
-			count += 1
-			content = scratchOff.content.upper()
+				count += 1
+				content = scratchOff.content.upper()
 
-			msg = msg.replace(content, str(spots[content]))
-			await msgSent.edit(content=msg)
+				msg = msg.replace(content, str(spots[content]))
+				await msgSent.edit(content=msg)
 
-			del spots[content]
+				del spots[content]
+
+		if skip == "skip":
+			msg = ''
+			msg += f"Winning number: {winningNumber}\n"
+			msg += f"{n[0]}\t{n[1]}\t{n[2]}\n"
+			msg += f"{n[3]}\t{n[4]}\t{n[5]}\n"
+			msg += f"{n[6]}\t{n[7]}\t{n[8]}\n"
+
+			msgSent = await ctx.send(msg)
 
 		profit = 0
 		for num in n:
@@ -54,6 +74,9 @@ class Scratch(commands.Cog):
 				profit += amntBet
 
 		await ctx.send(f"You won {profit}{self.coin}")
+
+		if profit != 0:
+			await self.bot.get_cog("Economy").addWinnings(ctx.author.id, amntBet + profit)
 
 
 def setup(bot):
