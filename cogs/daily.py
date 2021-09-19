cf58007dc56e7a216aabb39a03a8fd6b7b969ad5
PyCord3494 <wilsonis123@icloud.com>
2021-09-19 12:12:34 -0700

    Command cooldown removed. Now based on saved data in file

--- cogs/daily.py
+++ cogs/daily.py
@@ -4,6 +4,10 @@ import pymysql
 import asyncio
 import config
 
+import json
+import time
+import math
+
 class Daily(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
@@ -11,21 +15,40 @@ class Daily(commands.Cog):
 		self.coin = "<:coins:585233801320333313>"
 
 	@commands.command()
-	@commands.cooldown(1, 86400, commands.BucketType.user)
 	async def daily(self, ctx):
+		userId = ctx.author.id
+
 		if not await self.bot.get_cog("Economy").accCheck(ctx.author):
-			await ctx.send("Hello! Please type .start to create your wallet. :smiley:")
-			ctx.command.reset_cooldown(ctx)
-			return			
+			await ctx.invoke(self.bot.get_command('start'))
+		else:
+			with open(r"rewards.json", 'r') as f:
+				rewards = json.load(f)
+
+			if (str(userId) in rewards) and ('daily' in rewards[f'{userId}']):
+				if rewards[f'{userId}']['daily'] > time.time():
+					waittime = rewards[f'{userId}']['daily'] - time.time()
+					await ctx.send(f"Please wait **{math.floor(waittime/3600)}h {math.floor((waittime/60) % 60)}m** to use this again!")
+					return
+
+			elif not str(userId) in rewards:
+				rewards[f'{userId}'] = dict()
+			rewards[f'{userId}']['daily'] = time.time() + 86400
+
+			with open(r"rewards.json", 'w') as f:
+				json.dump(rewards, f, indent=4)
+
+
 		dailyReward = await self.getDailyReward(ctx)
 		multiplier = self.bot.get_cog("Economy").getMultiplier(ctx.author)
 		extraMoney = int(dailyReward * (multiplier - 1))
-		await self.bot.get_cog("Economy").addWinnings(ctx.author.id, dailyReward + extraMoney)
+		await self.bot.get_cog("Economy").addWinnings(userId, dailyReward + extraMoney)
 		balance = await self.bot.get_cog("Economy").getBalance(ctx.author)
 		embed = discord.Embed(color=1768431)
 		embed.add_field(name = f"You got {dailyReward} (+{extraMoney}) {self.coin}", 
 						value = f"You have {balance} credits\nMultiplier: {multiplier}x\nExtra Money: {extraMoney}", inline=False)
 		await ctx.send(embed=embed)
+
+
 
 	async def getDailyReward(self, ctx):
 		db = pymysql.connect(host=config.host, port=3306, user=config.user, passwd=config.passwd, db=config.db, autocommit=True)
