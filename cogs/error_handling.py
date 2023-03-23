8c19e52519a71cc196af9a46f6b8d16ed99d8e2f
PyCord3494 <jngraham00@gmail.com>
2023-03-23 01:10:23 +0000

    Removed unused handlings due to slash commands. Added new cooldown exception

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -8,6 +8,8 @@ from nextcord.ext.commands import has_permissions, MissingPermissions
 import asyncio
 import sys
 
+from cooldowns import CallableOnCooldown
+
 import datetime
 import traceback
 from difflib import get_close_matches
@@ -16,69 +18,27 @@ from difflib import get_close_matches
 class ErrorHandling(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		bot.event(self.on_application_command_error)
 
-	@commands.Cog.listener()
-	async def on_command_error(self, interaction:Interaction, error):
+	# @commands.Cog.listener()
+	async def on_application_command_error(self, interaction:Interaction, error):
 		embed = nextcord.Embed(title=f"{self.bot.user.name} | ERROR", color=0xFF0000)
 		error = getattr(error, 'original', error)
 
-		if "missing permissions" in str(error).lower() or "send message" in str(error).lower():
+		if "missing permissions" in str(error).lower() or "send message" in str(error).lower() or "missing access" in str(error).lower():
 			return
 
-		elif isinstance(error, commands.CommandNotFound):
-			if "Discord" in interaction.guild.name or "Discords.com" in interaction.guild.name:
-				return
-			lst = [	"background", "bal", "balance", "bank", "blackjack", "bg",
-					"coinflip", "claim", "colorguesser", "credits", "crash", "crate", "earn", "free", "freemoney", 
-					"level", "monthly", "position", "profile", "rewards", "roulette", "rps", 
-					"shop", "slot", "slots", "stats", "top", "vote", "weekly"]
-		#	embed.description = "Command not found!"
-			cmd = interaction.message.content.split()[0][1:]
-			try:
-				closest = get_close_matches(cmd.lower(), lst)[0]
-			except IndexError:
-				embed.description = f"`{cmd.lower()}` is not a known command."
-			else:
-				embed.description = f"`{cmd.lower()}` is not a command, did you mean `{closest}`?"
-
-		elif isinstance(error, commands.MissingRequiredArgument):
-			if interaction.command is None:
-				return
-			try:	
-				await interaction.invoke(self.bot.get_command(f'help {interaction.command.name}'))
-				interaction.command.reset_cooldown(interaction)
-				return
-			except:
-				err = str(error.param)
-				err = err.replace("_", " ")
-				err = str(err.split(":")[0])
-
-				firstChar = err[0]
-				if firstChar.lower() in "aeiou" and err != "user":
-					a_an = "an"
-				else:
-					a_an = "a"
-
-				if err == 'amntbet':
-					err = 'amount to bet'
-				elif err == 'sideBet':
-					err = 'side to vote for (heads or tails)'
-				embed.description = f"Please specify {a_an} {err} for this command to work."
-
-		elif isinstance(error, commands.TooManyArguments):
-			embed.description = "You have tried using this command with too many arguments."
+		elif isinstance(error, commands.CommandOnCooldown) or isinstance(error, CallableOnCooldown):
+			a = datetime.timedelta(seconds=error.retry_after)
+			cooldown = str(a).split(".")[0]
+			embed.description = f"{str(interaction.application_command.qualified_name).title()} is on cooldown. Please retry again in {cooldown}"
 
 		elif isinstance(error, commands.CheckFailure):
 			try:
-				await interaction.response.send_message(f"{error}")
+				await interaction.send(f"{error}")
 			except Exception:
 				pass
 			return
-
-		elif isinstance(error, commands.CommandOnCooldown):
-			a = datetime.timedelta(seconds=error.retry_after)
-			cooldown = str(a).split(".")[0]
-			embed.description = f"{str(interaction.command).title()} is on cooldown. Please retry again in {cooldown}"
 
 		elif isinstance(error, commands.BadArgument):
 			embed.description = f"{error}"
@@ -88,11 +48,11 @@ class ErrorHandling(commands.Cog):
 			err = err.split(':', 2)[-1]
 			
 			if err == "forbiddenError":
-				await interaction.response.send_message("Your Discord settings does not allow me to DM you. Please change them and try again.")
+				await interaction.send("Your Discord settings does not allow me to DM you. Please change them and try again.")
 				return
 
 			if err == "timeoutError":
-				await interaction.response.send_message("Did not respond in time; timeout.")
+				await interaction.send(f"Did not respond in time; timeout.")
 				return
 
 			exc = ''.join(traceback.format_exception(type(error), error, error.__traceback__, chain=False))
@@ -115,22 +75,27 @@ class ErrorHandling(commands.Cog):
 					await ch.send(f"{exc[1999:]}")
 			else:
 				await ch.send(f"{exc}")
-			await ch.send(f"Error.\nCommand message: {interaction.message.content}\nUser: {interaction.user.id}")
 
-			# await ch.send(embed=e)
-			# await interaction.response.send_message(f"{error}  {interaction.command.qualified_name}")
+			try:
+				await ch.send(f"Further info\n{interaction.application_command.qualified_name} ")
+			except Exception as e:
+				await ch.send(f"error with further infooooo: {e}")
+			
 
-		embed.set_thumbnail(url=interaction.user.avatar)
-		embed.set_footer(text=interaction.user)
+
+			embed.set_thumbnail(url=interaction.user.avatar)
+			embed.set_footer(text=interaction.user)
+
 
 		try:
-			await interaction.response.send_message(embed=embed)
+			await interaction.send(embed=embed)
+			return 
 		except Exception as e:
 			pass
 			
 			
-		if interaction.command is not None and not isinstance(error, commands.CommandOnCooldown):
-			interaction.command.reset_cooldown(interaction)
+		# if interaction.application_command is not None and not isinstance(error, commands.CommandOnCooldown) and not isinstance(error, CallableOnCooldown):
+		# 	interaction.application_command.reset_cooldown(interaction)
 
 def setup(bot):
 	bot.add_cog(ErrorHandling(bot))
