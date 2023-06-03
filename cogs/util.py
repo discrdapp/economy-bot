562e875c247ef926a840d6c76ee542a1cf86f355
Justin Graham <jngraham00@gmail.com>
2023-06-03 10:30:59 -0500

    Users now given 3 choice for Work cmd for which job they want

--- cogs/util.py
+++ cogs/util.py
@@ -4,33 +4,58 @@ from nextcord import Interaction
 
 import cooldowns, asyncio, random
 
+
+
+class Button(nextcord.ui.Button):
+	def __init__(self, label, style):
+		super().__init__(label=label, style=style)
+	
+	async def callback(self, interaction:Interaction):
+		self.view.stop()
+		self.view.job = self.label
+
+
 class Util(commands.Cog):
 	def __init__(self, bot):
-		self.bot = bot
+		self.bot:commands.bot.Bot = bot
 		self.jobs = ["Administrative Assistant", "Executive Assistant", "Marketing Manager", "Customer Service Representative", "Nurse Practitioner", "Software Engineer", "Sales Manager", "Data Entry Clerk", "Office Assistant", "Accounting Specialist", "Payroll Specialist", "Dentist", "Registered Nurse", "Pharmacist", "Computer Systems Analyst", "Physician", "Database Administrator", "Software Developer", "Physical Therapist", "Web Developer", "Dental Hygienist", "Occupational Therapist", "Veterinarian", "Computer Programmer", "School Psychologist", "Physical Therapist Assistant", "Interpreter & Translator", "Mechanical Engineer", "Veterinary Technologist & Technician", "Epidemiologist", "IT Manager", "Market Research Analyst", "Diagnostic Medical Sonographer", "Computer Systems Administrator", "Respiratory Therapist", "Medical Secretary", "Civil Engineer", "Substance Abuse Counselor", "Speech-Language Pathologist", "Landscaper & Groundskeeper", "Radiologic Technologist", "Cost Estimator", "Financial Advisor", "Marriage & Family Therapist", "Medical Assistant", "Lawyer", "Accountant", "Compliance Officer", "High School Teacher", "Clinical Laboratory Technician", "Maintenance & Repair Worker", "Bookkeeping, Accounting, & Audit Clerk", "Financial Manager", "Recreation & Fitness Worker", "Insurance Agent", "Elementary School Teacher", "Dental Assistant", "Management Analyst", "Home Health Aide", "Pharmacy Technician", "Construction Manager", "Public Relations Specialist", "Middle School Teacher", "Massage Therapist", "Paramedic", "Preschool Teacher", "Hairdresser", "Marketing Manager", "Patrol Officer", "School Counselor", "Executive Assistant", "Financial Analyst", "Personal Care Aide", "Clinical Social Worker", "Business Operations Manager", "Loan Officer", "Meeting, Convention & Event Planner", "Mental Health Counselor", "Nursing Aide", "Sales Representative", "Architect", "Sales Manager", "HR Specialist", "Plumber", "Real Estate Agent", "Glazier", "Art Director", "Customer Service Representative", "Logistician", "Auto Mechanic", "Bus Driver", "Restaurant Cook", "Child & Family Social Worker", "Administrative Assistant", "Receptionist", "Paralegal", "Cement Mason & Concrete Finisher", "Painter", "Sports Coach", "Teacher Assistant", "Brickmason & Blockmason", "Cashier", "Janitor", "Electrician", "Delivery Truck Driver", "Maid & Housekeeper", "Carpenter", "Security Guard", "Construction Worker", "Fabricator", "Telemarketer"]
-
-
-	async def getChoice(self, interaction:Interaction):
-		await interaction.send("Type something...")
-		def is_me(m):
-			return m.author == interaction.user
-		try:
-			msg = await self.bot.wait_for('message', check=is_me, timeout=10)
-		except asyncio.TimeoutError:
-			raise Exception("timeoutError")
-		return msg
+		# self.jobs = ["1", "2", "3", "4"]
 
 	@nextcord.slash_command()
-	@cooldowns.cooldown(1, 5400, bucket=cooldowns.SlashBucket.author)
+	@cooldowns.cooldown(1, 5400, bucket=cooldowns.SlashBucket.author, cooldown_id="work")
 	async def work(self, interaction:Interaction):
 		embed = nextcord.Embed(color=1768431, title=f"{self.bot.user.name} | Work")
+		embed.description = "What would you like to work as?"
 
-		job = random.choice(self.jobs)
+		jobs = random.sample(self.jobs, 3)
+
+		view = nextcord.ui.View(timeout=5)
+
+		job1 = Button(label=jobs[0], style=nextcord.ButtonStyle.green)
+		job2 = Button(label=jobs[1], style=nextcord.ButtonStyle.green)
+		job3 = Button(label=jobs[2], style=nextcord.ButtonStyle.green)
+
+		view.add_item(job1)
+		view.add_item(job2)
+		view.add_item(job3)
+
+		msg = await interaction.send(embed=embed, view=view, ephemeral=True)
+		# msg = newMsg.fetch()
+
+		if await view.wait():
+			embed.set_footer(text="Timed out. First job auto-picked")
+			view.job = job1.label
+
+		if not view.job:
+			print("not defined...")
+			return
+
 		pay = random.randrange(6000, 20001)
 		await self.bot.get_cog("Economy").addWinnings(interaction.user.id, pay)
 
-		embed.description = f"You worked as a {job} and earned {pay:,}<:coins:585233801320333313>"
-		await interaction.send(embed=embed)
+		aan = "an" if view.job[0].lower() in "aeiou" else "a"
+		embed.description = f"You worked as {aan} {view.job} and earned {pay:,}<:coins:585233801320333313>"
+		await msg.edit(embed=embed, view=None)
 
 
 	@nextcord.slash_command()
