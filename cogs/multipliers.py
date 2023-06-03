9b9fc5e51f144de75f25f5218a62db52f3055ba1
Justin Graham <jngraham00@gmail.com>
2023-06-03 10:36:06 -0500

    Functions now take just user ID instead of entire user object

--- cogs/multipliers.py
+++ cogs/multipliers.py
@@ -7,7 +7,7 @@ from db import DB
 
 class Multipliers(commands.Cog):
 	def __init__(self, bot):
-		self.bot = bot
+		self.bot:commands.bot.Bot = bot
     
 
 	def convertSecondsToTimeStr(self, seconds):
@@ -27,36 +27,36 @@ class Multipliers(commands.Cog):
 
 		return timeLeft
 	
-	def hasMultiplier(self, user):
-		expires = DB.fetchOne("SELECT Expires FROM Multipliers WHERE DiscordID = ?;", [user.id])
+	def hasMultiplier(self, userId:int):
+		expires = DB.fetchOne("SELECT Expires FROM Multipliers WHERE DiscordID = ?;", [userId])
 		if not expires:
 			return False
 		expireDate = datetime.datetime.strptime(expires[0], '%Y-%d-%m %H:%M:%S')
 		if expireDate < datetime.datetime.now():
-			DB.delete("DELETE FROM Multipliers WHERE DiscordID = ?;", [user.id])
+			DB.delete("DELETE FROM Multipliers WHERE DiscordID = ?;", [userId])
 			return False
 		
 		return True
 	
-	def addMultiplier(self, user, multiplierAmnt:int, expiration:datetime.datetime):
-		if self.hasMultiplier(user):
-			multiplier, currExpiration = self.getMultiplierAndExpiration(user, False)
+	def addMultiplier(self, userId, multiplierAmnt:int, expiration:datetime.datetime):
+		if self.hasMultiplier(userId):
+			multiplier, currExpiration = self.getMultiplierAndExpiration(userId, False)
 			if multiplier == multiplierAmnt:
                 # extend expiration
 				expirationSecondsLeft = (expiration - datetime.datetime.now()).total_seconds()
 				newTime = currExpiration + datetime.timedelta(seconds=expirationSecondsLeft)
-				DB.update("UPDATE Multipliers SET Expires = ? WHERE DiscordID = ? and Multiplier = ?;", [newTime.strftime('%Y-%d-%m %H:%M:%S'), user.id, multiplierAmnt])
+				DB.update("UPDATE Multipliers SET Expires = ? WHERE DiscordID = ? and Multiplier = ?;", [newTime.strftime('%Y-%d-%m %H:%M:%S'), userId, multiplierAmnt])
 				return "Successfully extended multiplier!"
 			else:
 				return "ERROR: You cannot stack multipliers. You can only extend your current one's time."
 
 		strExpiration = expiration.strftime('%Y-%d-%m %H:%M:%S')
-		DB.insert("INSERT INTO Multipliers(DiscordID, Multiplier, Expires) VALUES(?, ?, ?)", [user.id, multiplierAmnt, strExpiration])
+		DB.insert("INSERT INTO Multipliers(DiscordID, Multiplier, Expires) VALUES(?, ?, ?)", [userId, multiplierAmnt, strExpiration])
 		return "Successfully added multiplier!"
 
 
-	def getMultiplier(self, user):
-		multipliers = DB.fetchOne("SELECT Multiplier, Expires FROM Multipliers WHERE DiscordID == ?;", [user.id])
+	def getMultiplier(self, userId:int):
+		multipliers = DB.fetchOne("SELECT Multiplier, Expires FROM Multipliers WHERE DiscordID == ?;", [userId])
 		if not multipliers: 
 			return 1.0
 
@@ -67,12 +67,12 @@ class Multipliers(commands.Cog):
 		if expireDate > datetime.datetime.now(): # if multiplier not expired, return it
 			return multiplier
 		else:
-			DB.delete("DELETE FROM Multipliers WHERE DiscordID = ?;", [user.id])
+			DB.delete("DELETE FROM Multipliers WHERE DiscordID = ?;", [userId])
 
 		return 1.0
 
-	def getMultiplierAndExpiration(self, user, returnExpirationAsString=True):
-		multipliers = DB.fetchOne("SELECT Multiplier, Expires FROM Multipliers WHERE DiscordID == ?;", [user.id])
+	def getMultiplierAndExpiration(self, userId, returnExpirationAsString=True):
+		multipliers = DB.fetchOne("SELECT Multiplier, Expires FROM Multipliers WHERE DiscordID == ?;", [userId])
 		if not multipliers: 
 			return 1.0, None
 
