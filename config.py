4490f446837b620404400cba5713f4a22f4a10e6
PyCord3494 <jngraham00@gmail.com>
2023-07-03 16:47:07 +0000

    init commit

---
+++ config.py
@@ -0,0 +1,5 @@
+db = "justingraham_db.db"
+channelIDForErrors = 790282020009148446
+adminServerID = 585226670361804827
+channelToNotifyOfServerAddingBot = 790282431860047882
+botOwnerDiscordID = 547475078082985990
