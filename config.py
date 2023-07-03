0e5bab034807720830dba2abb69030cb76e2af76
PyCord3494 <jngraham00@gmail.com>
2023-07-03 20:03:58 +0000

    Added comments

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-db = "justingraham_db.db"
-channelIDForErrors = 790282020009148446
-adminServerID = 585226670361804827
-channelToNotifyOfServerAddingBot = 790282431860047882
-botOwnerDiscordID = 547475078082985990
+db = "justingraham_db.db" # name of db in root folder
+channelIDForErrors = 790282020009148446 # where bot should send (unexpected) errors to
+adminServerID = 585226670361804827 # discord server id for admin server
+channelToNotifyOfServerAddingBot = 790282431860047882 # place to send message to when bot is added to a server
+botOwnerDiscordID = 547475078082985990 # put your discord user id here. This needed for certain commands like deleting users and adding money/xp/items
