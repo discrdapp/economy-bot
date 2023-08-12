c580b1ecf79fe13d81ea37776ccc0fdb5300d584
Justin Graham <jngraham00@gmail.com>
2023-08-12 18:46:08 -0500

    Implemented logging. Added cooldown IDs. Changed crate probabilities

--- cogs/shop.py
+++ cogs/shop.py
@@ -4,9 +4,9 @@ from nextcord import Interaction
 
 from random import randint
 import cooldowns
-import datetime
 
 from db import DB, buyableItems, buyableItemNamesList, usableItemNamesList
+from cogs.totals import log
 
 
 ################
@@ -20,7 +20,7 @@ from db import DB, buyableItems, buyableItemNamesList, usableItemNamesList
 
 class MySource(menus.ListPageSource):
 	def __init__(self, data):
-		super().__init__(data, per_page=4)
+		super().__init__(data, per_page=5)
 
 	async def format_page(self, menu, entries):
 		# print(entries)
@@ -29,17 +29,18 @@ class MySource(menus.ListPageSource):
 		embed = nextcord.Embed(color=1768431, title=f"The Casino | Shop")
 
 		for x in range(0, len(entries)):
-			embed.add_field(name=f"{entries[x][1]} ─ {entries[x][4]:,}<:coins:585233801320333313>", value=f"{entries[x][2]}", inline=False)
+			embed.add_field(name=f"{entries[x][1]} {entries[x][7]} ─ {entries[x][4]:,}<:coins:585233801320333313>", value=f"{entries[x][2]}", inline=False)
+		
+		embed.set_footer(text=f"Page {menu.current_page + 1}/{self.get_max_pages()}")
 		return embed
 
 class Shop(commands.Cog):
 	def __init__(self, bot):
 		self.bot:commands.bot.Bot = bot
 		self.coin = "<:coins:585233801320333313>"
-		self.items = buyableItems
 
 		self.itemsDict = dict()
-		for item in self.items:
+		for item in buyableItems:
 			self.itemsDict[item[0]] = {
 				"Name": item[1],
 				"Description": item[2],
@@ -55,25 +56,26 @@ class Shop(commands.Cog):
 		pass
 
 	@shop.subcommand()
+	@cooldowns.cooldown(1, 5, bucket=cooldowns.SlashBucket.author, cooldown_id='list')
 	async def list(self, interaction:Interaction):
 		embed = nextcord.Embed(color=1768431, title=f"{self.bot.user.name} | Shop")
 
 		pages = menus.ButtonMenuPages(
-			source=MySource(self.items),
+			source=MySource(buyableItems),
 			clear_buttons_after=True,
 			style=nextcord.ButtonStyle.primary,
 		)
 		await pages.start(interaction=interaction)
 
 	@shop.subcommand()
-	@cooldowns.cooldown(1, 5, bucket=cooldowns.SlashBucket.author)
+	@cooldowns.cooldown(1, 5, bucket=cooldowns.SlashBucket.author, cooldown_id='buy')
 	async def buy(self, interaction:Interaction, 
 						itemSelected = nextcord.SlashOption(
 										required=True,
 										name="item", 
 										choices=buyableItemNamesList), 
 						amnt:int=1):
-		for item in self.items:
+		for item in buyableItems:
 			if item[1] == itemSelected:
 				theid = item[0]
 				break
@@ -91,7 +93,7 @@ class Shop(commands.Cog):
 			await interaction.send(embed=embed)
 			return
 		# not donator but trying to buy donator item
-		if theid == 7 and not self.bot.get_cog("Economy").isDonator(interaction.user.id):
+		if theid == 999 and not self.bot.get_cog("Economy").isDonator(interaction.user.id):
 			embed.description = f"That can only be used by Donators!"
 			await interaction.send(embed=embed)
 			return
@@ -115,15 +117,13 @@ class Shop(commands.Cog):
 				return
 
 
-
-
-		await self.bot.get_cog("Economy").addWinnings(discordId, -(cost))
+		logID = await self.bot.get_cog("Economy").addWinnings(discordId, 0, giveMultiplier=False, activityName=f"Bought {amnt} {itemSelected}", amntBet=cost)		
 		
 		if theid == 3:
 			DB.update("UPDATE Economy SET DailyReward = DailyReward + ? WHERE DiscordID = ?;", [amnt*1000, discordId])
 			embed.description = f"Successfully added {amnt * 1000}{self.coin} to daily reward"
 
-		elif theid == 7:
+		elif theid == 999:
 			DB.update("UPDATE Economy SET DonatorReward = DonatorReward + ? WHERE DiscordID = ?;", [amnt*1500, discordId])
 			embed.description = f"Successfully added {amnt * 1500}{self.coin} to donator reward"
 
@@ -135,7 +135,9 @@ class Shop(commands.Cog):
 			embed.description = f"Successfully bought {amnt} {self.itemsDict[theid]['Name']}"
 
 		if self.itemsDict[theid]["Type"] == "Usable":
-			embed.set_footer(text=f"Use this item with /use {itemSelected}")
+			embed.set_footer(text=f"Use this item with /use {itemSelected}\nLog ID: {logID}")
+		else:
+			embed.set_footer(text=f"Log ID: {logID}")
 
 		await interaction.send(embed=embed)
 
@@ -177,45 +179,58 @@ class Shop(commands.Cog):
 		moneyToAdd = 0
 
 		# open amnt number of crates
+		embed.description = ""
 		for x in range(1, amnt+1):
 			# 30% chance to get crates
 			# 30% chance to get keys
 			# 20% chance to get 5000 - 12500 credits
 			# 20% chance to get 0 - 500 credits
-			choice = randint(1, 10)
-			if choice <= 3:
+			choice = randint(0, 100)
+			if choice <= 50: # 50% chance to find random item.
+				itemName, emoji = self.bot.get_cog("Inventory").getRandomItem()
+				self.bot.get_cog("Inventory").addItemToInventory(interaction.user.id, 1, itemName)
+				aan = "an" if itemName in "aeiou" else "a"
+				embed.description += f"You found {aan} {itemName} {emoji}"
+			elif choice <= 60:
 				amntToAdd = randint(0, 3)
-				embed.description = f"You found {amntToAdd} crates!"
+				embed.description += f"You found {amntToAdd} crates!\n"
 				crates += amntToAdd
 
-			elif choice <= 6:
+			elif choice <= 70:
 				amntToAdd = randint(1, 3)
-				embed.description = f"You found {amntToAdd} keys!"
+				embed.description += f"You found {amntToAdd} keys!\n"
 				keys += amntToAdd
 
-			elif choice <= 8:
-				bal = randint(5000, 12500)
-				embed.description = f"You found {bal}{self.coin}!"
+			elif choice <= 75:
+				bal = randint(2500, 12000)
+				embed.description += f"You found {bal}{self.coin}!\n"
 				moneyToAdd += bal
 
-			elif choice <= 10:
-				bal = randint(0, 500)
-				embed.description = f"You found {bal}{self.coin}!"
+			elif choice <= 90:
+				bal = randint(50, 250)
+				embed.description += f"You found {bal}{self.coin}!\n"
 				moneyToAdd += bal
+			else:
+				embed.description = "The crate was empty... Better luck next time!\n"
 
 			if x >= 25 and x % 25 == 0: # send message every 25 crates opened
 				await interaction.send(embed=embed)
 				embed = nextcord.Embed(color=1768431, title=f"{self.bot.user.name} | Shop")
 				embed.set_thumbnail(url=interaction.user.avatar)
 
+		logID = None
+		if moneyToAdd > 0:
+			logID = await self.bot.get_cog("Economy").addWinnings(interaction.user.id, moneyToAdd, giveMultiplier=False, activityName="Crate", amntBet=0)
 		if not x % 25 == 0: # send message for remaining crates that were opened, if not sent already
+			if logID:
+				embed.set_footer(text=f"Log ID: {logID}")
 			await interaction.send(embed=embed)
 
-		await self.bot.get_cog("Economy").addWinnings(interaction.user.id, moneyToAdd)
 		if crates > 0:
 			self.bot.get_cog("Inventory").addItemToInventory(interaction.user.id, crates, 'Crate')
 		if keys > 0:
 			self.bot.get_cog("Inventory").addItemToInventory(interaction.user.id, keys, 'Key')
+		
 		await self.bot.get_cog("Economy").balance(interaction)
 
 
