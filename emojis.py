58aeef2932598298d3d57b8434ba9f84931fc72f
Justin Graham <jngraham00@gmail.com>
2023-09-04 16:08:19 -0500

    Renamed from emojiss.py

---
+++ emojis.py
@@ -0,0 +1,18 @@
+# used for displaying the progress bar for active /quests
+bf = "<:bottom_filled:894039100926070784>"
+brf = "<:bottom_right_filled:894039100858961941>"
+tr = "<:top_right:894039100854767628>"
+trf = "<:top_right_filled:894039100804452382>"
+tl = "<:top_left:894039100821209148>"
+tlf = "<:top_left_filled:894039100821229578>"
+bl = "<:bottom_left:894039100745744415>"
+blf = "<:bottom_left_filled:894039100808626196>"
+t = "<:top:894039100733157388>"
+tf = "<:top_filled:894039100754133022>"
+b = "<:bottom:894039100666052630>"
+br = "<:bottom_right:894039100523421738>"
+
+coin = "<:coins:585233801320333313>"
+bitcoinEmoji = "<:bitcoin:1143401555656196237>"
+litecoinEmoji = "<:Litecoin:1143402028832407633>"
+ethereumEmoji = "<:Ethereum:1143402400581959691>"
