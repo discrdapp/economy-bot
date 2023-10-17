9d0255c53577b1a1f14576c774f9f9fce702d024
Justin Graham <jngraham00@gmail.com>
2023-10-17 17:37:45 -0500

    Added funcs to get/add diff File emojis

--- emojis.py
+++ emojis.py
@@ -1,3 +1,6 @@
+from nextcord import File, Interaction
+from random import randint
+
 # used for displaying the progress bar for active /quests
 bf = "<:bottom_filled:894039100926070784>"
 brf = "<:bottom_right_filled:894039100858961941>"
@@ -16,3 +19,43 @@ coin = "<:credits:1102297413357686965>"
 bitcoinEmoji = "<:bitcoin:1143401555656196237>"
 litecoinEmoji = "<:Litecoin:1143402028832407633>"
 ethereumEmoji = "<:Ethereum:1143402400581959691>"
+
+async def SendInteractionWithWave(interaction:Interaction, embed):
+	if randint(0, 1) == 0:
+		file = File("images/wumpus/pokerwumpus.png", filename="helpImage.png")
+	else:
+		file = File("images/wumpus/blackjackwumpus.png", filename="helpImage.png")
+
+	embed.set_thumbnail(url="attachment://helpImage.png")
+	await interaction.send(embed=embed, file=file)
+
+async def SendInteractionWithStop(interaction:Interaction, embed, ephemeral:bool):
+	file = File("images/wumpus/stop.png", filename="stopImage.png")
+
+	embed.set_thumbnail(url="attachment://stopImage.png")
+	await interaction.send(embed=embed, file=file)
+
+async def SendInteractionWithError(interaction:Interaction, embed, ephemeral:bool):
+	file = File("images/wumpus/error.png", filename="errorImage.png")
+
+	embed.set_thumbnail(url="attachment://errorImage.png")
+	await interaction.send(embed=embed, file=file)
+
+def GetWin():
+	return File("images/wumpus/win.png", filename="results.png")
+
+def GetLose():
+	return File("images/wumpus/lose.png", filename="results.png")
+
+
+def GetRandomFace():
+	x = randint(0, 2)
+
+	if x == 0:
+		file = File("images/wumpus/disappointed.png", filename="randomImage.png")
+	elif x == 1:
+		file = File("images/wumpus/sus.png", filename="randomImage.png")
+	elif x == 2:
+		file = File("images/wumpus/thinking.png", filename="randomImage.png")
+	
+	return file
