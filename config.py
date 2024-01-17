01c45e35e04b4f8c66b203509aaee52b98875e40
Justin Graham <jngraham00@gmail.com>
2024-01-17 12:12:58 -0600

    Added other channels needed

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 db = "justingraham_db.db" # name of db in root folder
-channelIDForErrors = 790282020009148446 # where bot should send (unexpected) errors to
-adminServerID = 585226670361804827 # discord server id for admin server
-channelToNotifyOfServerAddingBot = 790282431860047882 # place to send message to when bot is added to a server
-botOwnerDiscordID = 547475078082985990 # put your discord user id here. This needed for certain commands like deleting users and adding money/xp/items
+channelIDForErrors = 123456789 # where bot should send (unexpected) errors to
+channelIDForSuggestions = 123456789 # channel for people's /feedback to go to
+channelIDForBitcoin = 123456789 # look at support server for reference
+channelIDForMonopoly = 123456789 # look at support server for reference
+channelIDForCodes = 123456789 # look at support server for reference
+channelIDForRanks = 123456789 # look at support server for reference
+messageIDForRanks = 123456789 # use /copy to have bot send a msg in the channel from above, then copy that message ID and put it here
+adminServerID = 123456789 # discord server id for admin server
+channelToNotifyOfServerAddingBot = 123456789 # place to send message to when bot is added to a server
+botOwnerDiscordID = 123456789 # put your discord user id here. This needed for certain commands like deleting users and adding money/xp/items
+cryptoAPIKey = "1a2b3c4d5e6f-ab12-1234-1234-abc" # you'll need to make account and get api key from https://coinmarketcap.com/
