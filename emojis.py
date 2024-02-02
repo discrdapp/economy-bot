add73f08ecc530c18a91a41459c65a47b5c7d0a2
Justin Graham <jngraham00@gmail.com>
2024-02-02 17:36:52 -0600

    Added new emojis for one-line progress bar

--- emojis.py
+++ emojis.py
@@ -15,6 +15,14 @@ tf = "<:top_filled:894039100754133022>"
 b = "<:bottom:894039100666052630>"
 br = "<:bottom_right:894039100523421738>"
 
+small_l = "<:small_l:1203108335021461545>"
+small_lf = "<:small_lf:1203108336468369459>"
+small_r = "<:small_r:1203108346266386493>"
+small_rf = "<:small_rf:1203108348329861190>"
+small_m = "<:small_m:1203108337802027008>"
+small_mf = "<:small_mf:1203108339102523422>"
+
+
 coin = "<:credits:1102297413357686965>"
 bitcoinEmoji = "<:bitcoin:1143401555656196237>"
 litecoinEmoji = "<:Litecoin:1143402028832407633>"
